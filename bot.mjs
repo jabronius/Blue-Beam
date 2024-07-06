e95df74515ad0ac390b4d496dc19e797443a7ca9
Aaron McKannan <32783964+jabronius@users.noreply.github.com>
2024-07-06 19:12:46 -0400

    Update bot.mjs

--- bot.mjs
+++ bot.mjs
@@ -1,22 +1,18 @@
 import Web3 from 'web3';
 import dotenv from 'dotenv';
 import { Telegraf, Markup } from 'telegraf';
+import { config } from './config.mjs';
 import { handleStart, handleCallbackQuery } from './keyboardHandlers.mjs';
 
 dotenv.config();  // Load environment variables from .env file
 
-const telegramApiKey = process.env.TELEGRAM_API_KEY;
-const cronosRpcUrl = process.env.CRONOS_NODE_URL;
-const devAccount = process.env.DEV_ACCOUNT_ADDRESS;
-const privateKey = process.env.PRIVATE_KEY; // Ensure PRIVATE_KEY is set in .env if needed
-
 // Validate that the variables are loaded correctly
-console.log('TELEGRAM_API_KEY:', telegramApiKey);
-console.log('CRONOS_NODE_URL:', cronosRpcUrl);
-console.log('DEV_ACCOUNT_ADDRESS:', devAccount);
+console.log('TELEGRAM_API_KEY:', config.telegramApiKey);
+console.log('CRONOS_NODE_URL:', config.cronosNodeUrl);
+console.log('DEV_ACCOUNT_ADDRESS:', config.devAccount);
 
-const web3 = new Web3(new Web3.providers.HttpProvider(cronosRpcUrl));
-const bot = new Telegraf(telegramApiKey);
+const web3 = new Web3(new Web3.providers.HttpProvider(config.cronosNodeUrl));
+const bot = new Telegraf(config.telegramApiKey);
 
 bot.start(handleStart);
 bot.on('callback_query', handleCallbackQuery);
